dcf99886ca48774798a8f72a5ae1d922195bc75e
Sandeep Singh <sandeep.singh@williamhill.com.au>
2019-02-25 16:02:38 +1100

    First Cut for selenium WebDriver and xUnit

--- SeleniumFramework/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace SeleniumFramework
-{
-    public class Class1
-    {
-    }
-}
---
+++ SeleniumFramework/Tests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Xunit;
+
+namespace SeleniumFramework
+{
+    public class Tests : IDisposable
+    {
+        public IWebDriver driver;
+
+        public Tests()
+        {
+            try
+            {
+                driver = new ChromeDriver();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception while starting Browser " + e.InnerException);
+            }
+        }
+
+
+        [Fact]
+        public void ValidateTitle()
+        {
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://beteasy.com.au/sports-betting");
+            Assert.Equal("Sports Betting & Odds Online | BetEasy", driver.Title);
+        }
+
+
+
+
+        public void Dispose()
+        {
+            driver.Quit();
+        }
+
+
+    }
+}
