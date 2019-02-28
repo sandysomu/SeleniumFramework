379cd708fc17d3919f5111afbeee395e6b87de56
Sandeep Singh <sandeep.singh@williamhill.com.au>
2019-02-28 15:35:44 +1100

    add temp project to be removed later on

---
+++ TempProject/Functionaltests.cs
@@ -0,0 +1,25 @@
+using System;
+using TempProject.Selenium;
+using Xunit;
+
+namespace TempProject
+{
+    public class FunctionalTests
+    {
+        private static string url = "";
+
+
+        [Fact]
+        public void Test1()
+        {
+            Browser.GoTo(url);
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ TempProject/Selenium/Browser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TempProject.Selenium
+{
+    public static class Browser
+    {
+
+        static readonly IWebDriver _driver = new ChromeDriver();
+
+        public static IWebDriver Driver => _driver;
+
+
+        public static void GoTo(string url)
+        {
+            _driver.Navigate().GoToUrl(url);
+        }
+    }
+}
